4f9578dfad334e0abe2a32b38de37340c6a2f96f
Shakir <shakirc2983@gmail.com>
2025-02-24 18:40:56 +0000

    starting another testcase

--- tests/test_monte_carlo_simulation.py
+++ tests/test_monte_carlo_simulation.py
@@ -4,7 +4,7 @@ import pandas as pd
 from yfinance.exceptions import YFTickerMissingError
 from portfolio_opt.monte_carlo_simulation import MonteCarloSimulation
 from portfolio_opt.exceptions import TickerDateOutOfRange
-
+import numpy as np
 
 class TestMCS(unittest.TestCase):
 
@@ -19,7 +19,7 @@ class TestMCS(unittest.TestCase):
             str(context.exception), "Incorrect date format, should be YYYY-MM-DD"
         )
 
-    def test_date_out_of_range(self):
+    def test_date_incorrect_range(self):
         start_date = "2023-01-02"
         end_date = "2022-01-02"
         tickers = ["AAPL", "MSFT"]
@@ -43,5 +43,8 @@ class TestMCS(unittest.TestCase):
 
         self.assertEqual(context.exception.ticker, expected_ticker)
         self.assertEqual(context.exception.rationale, expected_rationale)
+
+    # TODO: // Create test cases for trying to see if the results of get_expected_returns and get_volatility is the right one produced
+    #       // Run functions and see if results produced is the same
 
 
