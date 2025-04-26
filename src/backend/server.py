41ffcc5bbdbe45dbcbd646dbdc96f1dde8f6bde2
Shakir <shakirc2983@gmail.com>
2025-04-26 18:28:24 +0100

    flask+react connection established

--- src/backend/server.py
+++ src/backend/server.py
@@ -1,10 +1,12 @@
 from flask import Flask, jsonify
+from flask_cors import cross_origin
 
 app = Flask(__name__)
 
-@app.route("/mcs")
+@app.route("/mcs", methods=['GET'])
+@cross_origin()
 def mcs():
     return jsonify({"mcs": ["Test1", "Test2", "Test3"]})
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0")
+    app.run(debug=True, host='0.0.0.0', port=5000)
